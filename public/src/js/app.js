4744079f66231ab14b18d704dca72788d579744b
mbittermann <marvin.bittermann@eves-it.de>
2020-02-03 12:25:33 +0100

    update app.js form submit function

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,7 +4,12 @@ const axios = require('axios');
 const form = document.getElementById('form');
 
 function submit(){
-    form.submit();
+    var i = document.getElementById('url');
+    if(i.value !== ''){
+        form.submit();
+    }else{
+        alert('You have to enter valid youtube url');
+    }
 }
 
 function resetInput(){
