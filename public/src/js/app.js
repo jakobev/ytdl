b04c94b58c815f76ba518d526767a49f0f853a25
mbittermann <marvin.bittermann@eves-it.de>
2020-02-03 13:42:25 +0100

    hghg

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,23 +3,26 @@ const axios = require('axios');
 
 const form = document.getElementById('form');
 
+window.onload = function(){
 
-function schick(){
-    console.log("fkapeokfgapofkgeapgfkea");
-    var i = document.getElementById('url');
-    console.log("input ", i.value);
-    if(i.value !== '' && i.value){
-        form.submit();
-    }else{
-        alert('You have to enter valid youtube url');
-        return;
+    
+    function schick(){
+        console.log("fkapeokfgapofkgeapgfkea");
+        var i = document.getElementById('url');
+        console.log("input ", i.value);
+        if(i.value !== '' && i.value){
+            form.submit();
+        }else{
+            alert('You have to enter valid youtube url');
+            return;
+        }
     }
-}
-
-function resetInput(){
-    console.log("lalalala");
-    var i = document.getElementById('url');
-    console.log("input ", i.value);
-    const input = document.getElementById('url');
-    input.value = '';
+    
+    function resetInput(){
+        console.log("lalalala");
+        var i = document.getElementById('url');
+        console.log("input ", i.value);
+        const input = document.getElementById('url');
+        input.value = '';
+    }
 }
