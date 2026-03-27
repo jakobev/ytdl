173534c9085a08cc4eb543957febcb5c65486683
jakobev <jakobev1@gmail.com>
2026-03-27 22:37:29 +0100

    Update app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -26,3 +26,29 @@ const axios = require('axios');
         const input = document.getElementById('url');
         input.value = '';
     }
+
+async function checkUrl(url) {
+        var status = document.getElementById("server-status-text")
+        try {
+            const response = await fetch(url, {method: 'GET'});
+
+            if (response.ok) {
+                console.log("Server status: Online");
+                status.style.color = "#00FF00";
+                status.textContent = "Online";
+                
+                return true;
+            } else {
+                console.log("Server status: " + response.status);
+                return false;
+            }
+        } catch (error){
+            console.error("Server Status: Offline");
+            status.style.color = "#FF0000"
+            status.textContent = "Offline";
+        }
+
+    }
+    document.getElementById("check-server-btn").addEventListener("click", function(){
+        checkUrl("https://heaven-serpentiform-olinda.ngrok-free.dev");
+    });
